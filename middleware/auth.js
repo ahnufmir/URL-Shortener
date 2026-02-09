d6991e0a61d4ad75b129fa7490071347c459f926
Ahnuf Mir <mirahnaf55@gmail.com>
2026-02-09 20:30:35 +0500

    added another auth via auth bearer token for non browser devices

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,32 @@
-const { getUser } = require('../service/auth')
+const { getUser } = require("../service/auth");
 
-async function restrictToLoggedinUsers(req,res,next) {
-    const userCookie = req.cookies.token;
-    if(!userCookie) return res.redirect('/url/login');
-
-    const user = getUser(userCookie);
-    if(!user) return res.redirect('/url/login');
-
-    req.user = user;
-    next();
-}
-async function checkAuth(req,res,next) {
-    const userCookie = req.cookies.token;
-
-    const user = getUser(userCookie);
-    //if(!user) return null;
-    req.user = user;
-    next();
+function checkLogin(req){
+      if (req.cookies) {
+      const userCookie = req.cookies.token;
+      return userCookie;
+    }
+    if (req.headers.authorization) {
+      const authHeader = req.headers.authorization.split("Bearer ")[1];
+      return authHeader;
+    }
 }
 
-module.exports = {restrictToLoggedinUsers, checkAuth};
+async function restrictToLoggedinUsers(req, res, next) {
+  const token = checkLogin(req);
+  if (!token) return res.json({ msg: "No token" });
+
+  const user = getUser(token);
+  if (!user) return res.redirect("/url/login");
+
+  req.user = user;
+  next();
+}
+async function checkAuth(req, res, next) {
+  const userCookie = checkLogin(req);
+  const user = getUser(userCookie);
+  //if(!user) return null;
+  req.user = user;
+  next();
+}
+
+module.exports = { restrictToLoggedinUsers, checkAuth };
